7355aa2cb4ba0bb32eef2a4b14961db03ac7b404
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-08 17:33:33 +0200

    ADD: abstract class Animale

---
+++ src/Animale.java
@@ -0,0 +1,9 @@
+public abstract class Animale {
+    protected void dormi() {
+        System.out.println("Zzz");
+    }
+
+    protected abstract void verso();
+
+    protected abstract void mangia();
+}
